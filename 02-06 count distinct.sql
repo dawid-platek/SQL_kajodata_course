f79809d1043cd51e5eed90edc487605d44e36339
dawidplatek <dawidplatek.eu.pl@outlook.com>
2024-09-28 12:00:09 +0200

    Create 02-06 count distinct.sql

---
+++ 02-06 count distinct.sql
@@ -0,0 +1,12 @@
+select
+	shipping_mode 
+	,count(order_id) 				as nr_of_orders
+	,count(distinct customer_id) 	as nr_of_unique_customers
+from orders o
+where delivery_city = 'Los Angeles'
+group by 1;
+
+select distinct 
+shipping_mode 
+,delivery_state 
+from orders
