540d0d6cbb8049d77381cd48b7b4970a69ad147f
dawidplatek <dawidplatek.eu.pl@outlook.com>
2024-10-01 23:25:30 +0200

    Update 03-02 left join.sql

--- 03-02 left join.sql
+++ 03-02 left join.sql
@@ -1,6 +1,7 @@
 select 
-o.customer_id
-,or2.order_id
+o.customer_id as customer
+,o.order_id   as original_order
+,or2.order_id as returned_order
 ,or2.next_order_free
 from orders o
 left join order_returns or2 on o.order_id = or2.order_id
