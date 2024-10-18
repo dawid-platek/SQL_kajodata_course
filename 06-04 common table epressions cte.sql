135d6524bb1e47d1d9273e16281b87aca50dc205
dawidplatek <dawidplatek.eu.pl@gmail.com>
2024-10-18 21:15:36 +0200

    Update 06-04 common table epressions cte.sql

--- 06-04 common table epressions cte.sql
+++ 06-04 common table epressions cte.sql
@@ -24,20 +24,25 @@ ds_sm_orders as
 		select 
 		delivery_state 
 		,shipping_mode 
-		,count(o.order_id) 	as nr_of_orders_ds_sm
+		,count(o.order_id) 	as orders_ds_sm
 		from orders o 
 		group by 1, 2
 	)
-select * from ds_sm_orders
+select
+	dss.delivery_state
+	,dss.shipping_mode
+	,dso.orders_ds
+	,dss.orders_ds_sm
+	,round((dss.orders_ds_sm / dso.orders_ds) * 100,1) 	as ds_ratio_percent
+from ds_sm_orders dss
+join ds_orders dso on dss.delivery_state = dso.delivery_state
+join needed_states 		ns on ns.delivery_state = dss.delivery_state
+cross join all_orders 	ao 
+order by 1, 2
 
 
--- do dokończenia !!!
 
-
-
-
-
-select 
+/* select 
 o.delivery_state 
 ,o.shipping_mode 
 ,(select count(order_id) as total_orders from orders) 		as total_orders
@@ -63,6 +68,6 @@ where
 		having count(distinct customer_id) > 100
 	)
 group by 1, 2, 3, 4
-order by 1, 2
+order by 1, 2 */
 
 
