c1cef717ab961ed5e18bc4ef47ce4a71140583b4
dawidplatek <dawidplatek.eu.pl@gmail.com>
2024-10-26 18:18:34 +0200

    Create 07-03 2 tasks.sql

---
+++ 07-03 2 tasks.sql
@@ -0,0 +1,32 @@
+/* % distribution of clients orders as histogram */
+
+
+with
+orders_per customer as
+	(
+	select 
+		customer_id 
+		,count(order_id) as orders
+	from orders
+	group by 1
+	),
+all_customers as
+	(
+	select count(distinct customer_id) as total_customers
+	from orders
+	),
+customers_histogram as
+	(
+	select
+		orders
+		,count(customer_id) as customers
+	from orders_per_customer
+	group by 1
+	)
+select
+	ch.orders
+	,round((ch.customers / ac.total_customers)*100, 2) as prc_distribution
+from customers_histogram ch
+cross join all_customers ac
+order by 1
+
